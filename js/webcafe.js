6cfcf7ed10912179a331ef07c116dd1d13f07f02
hwan02 <ssh32009757@gmail.com>
2017-07-07 16:23:27 +0900

    인기 사이트 마크업 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -26,7 +26,13 @@ $(function() {
         $(this).parent().addClass("act").siblings().removeClass("act");
     });
 });
-
+//  관련사이트 키보드 포커스 시 클래스 추가 및 제거
+$(".related-list a").focusin(function() {
+    $(this).parents(".related-list").addClass("list-act");
+});
+$(".related-list a").focusout(function() {
+    $(this).parents(".related-list").removeClass("list-act");
+});
 // 서브메뉴 제어를 위한 스크립트
 // $(function(){
 //   $(".main-menu > li").hover(
